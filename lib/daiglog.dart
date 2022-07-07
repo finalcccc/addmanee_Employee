4ae9469231d29faa4b8edd3dc989f3fca5edf25e
Final final <finalp58@gmail.com>
2022-07-07 20:09:47 +0700

    d

--- lib/daiglog.dart
+++ lib/daiglog.dart
@@ -3,12 +3,11 @@ import 'package:flutter/material.dart';
 Dialog_Cire(BuildContext context) {
   return showDialog(
     context: context,
-    builder: (context) => SingleChildScrollView(
-      child: Center(
+    builder: (context) =>Center(
         child: CircularProgressIndicator(
           color: Colors.green,
         ),
       ),
-    ),
+
   );
 }
