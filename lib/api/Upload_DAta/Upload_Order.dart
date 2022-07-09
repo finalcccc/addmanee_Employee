8dddd7fd002495db774c9a627611e6068aaea854
Final final <finalp58@gmail.com>
2022-07-09 11:20:29 +0700

    d

--- lib/api/Upload_DAta/Upload_Order.dart
+++ lib/api/Upload_DAta/Upload_Order.dart
@@ -1,5 +1,7 @@
 // ignore_for_file: non_constant_identifier_names, avoid_function_literals_in_foreach_calls, await_only_futures
 
+import 'dart:math';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:untitled4/notifire/Cartnotififire.dart';
 import 'package:untitled4/notifire/employeeNotifire.dart';
@@ -20,26 +22,24 @@ Upload_Order(
         .doc(f.Product_ID)
         .update({'amount': FieldValue.increment(-f.amout!)});
   });
-  Order.nameCutommer = cart.nameCutomer;
-  Order.tel = cart.tel;
-  Order.Employee_ID = emp.CurrentEmployee_loco!.id;
-  Order.address = cart.address;
-  Order.sumtotal = cart.sumltoal;
-  Order.amouttotal = cart.amoultoal;
-  CollectionReference rfn =
-      await FirebaseFirestore.instance.collection('order');
-  DocumentReference duoid = await rfn.add(Order.toMap());
-  Order.id = duoid.id;
-  duoid.set(Order.toMap()).then((value) async {
-    cart.Carts = [];
-    cart.price = [];
-    cart.amoul = [];
-    cart.amoultoal = 0;
-    cart.sumltoal = 0;
-    cart.nameCutomer = '';
-    cart.address = '';
-    cart.tel = '020';
-    cart.Refresh();
-    GetProduct(product);
+  int randomNumber =await Random().nextInt(90) + 1000000;
+  Order.id = await randomNumber.toString();
+  Order.nameCutommer = await cart.nameCutomer;
+  Order.tel = await cart.tel;
+  Order.Employee_ID =await emp.CurrentEmployee_loco!.id;
+  Order.address =await cart.address;
+  Order.sumtotal = await cart.sumltoal;
+  Order.amouttotal = await cart.amoultoal;
+  FirebaseFirestore.instance.collection('order').doc(randomNumber.toString()).set(Order.toMap()).then((value){
+      cart.Carts = [];
+      cart.price = [];
+      cart.amoul = [];
+      cart.amoultoal = 0;
+      cart.sumltoal = 0;
+      cart.nameCutomer = '';
+      cart.address = '';
+      cart.tel = '020';
+      cart.Refresh();
+      GetProduct(product);
   });
 }
